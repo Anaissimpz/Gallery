20517b4c4558c87febc5b25d810b97431d765fdd
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-15 16:45:05 +0200

    add share function in views.py

--- album/views.py
+++ album/views.py
@@ -26,3 +26,6 @@ def location(request,loc):
     images=Image.get_location_images(loc=loc)
     return render(request, 'pictures/location.html',{"pics": images})
 
+def share(request,id):
+    image=Image.share(id=id)
+    return redirect(pic_of_day)
