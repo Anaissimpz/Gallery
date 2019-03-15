fd2148aca90db84d986ea0aa202fc0ad2cdbe9cb
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-15 13:24:54 +0200

    create model of the location

--- album/models.py
+++ album/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+
+class Location(models.Model):
+     country=models.CharField(max_length=50)
+     city=models.CharField(max_length=50)
+     place=models.CharField(max_length=50,default='unkown',null=True)
+     def __str__(self):
+        return self.country
+     def save_location(self):
+         self.save()
+     def delete_location(self):
+      self.delete()
