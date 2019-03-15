06639ba336d10cd7c2c39824ca4482d79d373893
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-15 13:25:58 +0200

    add function of picture_of_day

--- album/views.py
+++ album/views.py
@@ -5,3 +5,8 @@ import datetime as dt
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
+def picture_of_day(request):
+    
+    date = dt.date.today()
+    pics = Image.objects.all()
+    return render(request, 'pictures/home.html', {"date": date,"pics":pics})
