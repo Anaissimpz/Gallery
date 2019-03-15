3cc4863c64074900dad54f2f4ccb4ea17c37c4a0
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-15 16:43:06 +0200

    add search result function in views.py

--- album/views.py
+++ album/views.py
@@ -10,3 +10,16 @@ def picture_of_day(request):
     date = dt.date.today()
     pictures = Image.objects.all()
     return render(request, 'all-pictures/home.html', {"date": date,"pictures":pictures})
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.get_category_images(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'pictures/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'pictures/search.html',{"message":message})
+
