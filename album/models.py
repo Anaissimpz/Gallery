da163c9c58abb748fa013c88438b64ef35359e8a
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-15 15:00:20 +0200

    add function of updating location

--- album/models.py
+++ album/models.py
@@ -3,12 +3,17 @@ import datetime as dt
 
 
 class Location(models.Model):
-     country=models.CharField(max_length=50)
-     city=models.CharField(max_length=50)
-     place=models.CharField(max_length=50,default='unkown',null=True)
-     def __str__(self):
+    country=models.CharField(max_length=20)
+    city=models.CharField(max_length=20)
+    place=models.CharField(max_length=20,default='unkown',null=True)
+    def __str__(self):
         return self.country
-     def save_location(self):
-         self.save()
-     def delete_location(self):
-      self.delete()
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
+    @classmethod
+    def update_all(cls,id,new,newer,newest):
+        location=Location.objects.filter(id=id)
+        location.update(country=new,city=newer,place=newest)
+        return location
