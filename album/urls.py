81b440e0453a17ee66182dcbaba10b436edfaf4c
Anaissimpz <anaissimpenzwe@gmail.com>
2019-03-15 13:08:06 +0200

    create urls.py file in album

--- album/urls.py
+++ album/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.index, name = 'home'),
+    url('^$',views.picture_of_day,name='pictureToday'),
 ]
